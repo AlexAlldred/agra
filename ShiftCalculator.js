b9f297e60d81b64eea6ff6a177df84683d42e1ab
Alex Alldred <alexalldred@gmail.com>
2023-05-25 14:21:51 -0400

    Revert "New UI, Enhanced search function"
    This reverts commit f035a4b8e75ed535961027aef6066d241a35bf2e.

--- ShiftCalculator.js
+++ ShiftCalculator.js
@@ -1,39 +1,25 @@
 
-var monthAndYear = document.getElementById("monthAndYear");
-var tomorrow = document.getElementById("tomorrow");
-var yesterday = document.getElementById("yesterday");
-var yesterdayLabel = document.getElementById("yesterdayLabel");
-var tomorrowLabel = document.getElementById("tomorrowLabel");
-var todayLabel = document.getElementById("todayLabel");
-
-
+monthAndYear = document.getElementById("monthAndYear");
 checkLabel = document.getElementById("checkLabel");
 const sun = "sun_max_fill.png";
 const moon = "moon_stars_fill.png";
 const vacay = "Gift Fill.png";
-var vacation = [new Date(2023, 7, 1), new Date(2023, 7, 2), new Date(2023, 7, 3),new Date(2023, 7, 7), new Date(2023, 7, 8),new Date(2023, 7, 9), new Date(2023, 6, 20), new Date(2023, 6, 21), new Date(2023, 6, 22)];
+var vacation = [new Date(2023, 7, 1)];
 
 const date1 = new Date();
-let firstFlag = false;
+
 let day1 = date1.getDate();
 let month1 = date1.getMonth() + 1;
 let year1 = date1.getFullYear();
 
-if(!firstFlag)
-{
-    dayCheck = document.getElementById("checkDay");
-    monthCheck = document.getElementById("checkMonth");
-    yearCheck = document.getElementById("checkYear");
-    firstFlag = true;
-}
+dayCheck = document.getElementById("checkDay");
+monthCheck = document.getElementById("checkMonth");
+yearCheck = document.getElementById("checkYear");
+
 // This arrangement can be altered based on how we want the date's format to appear.
-const yesterdaysDate = month1 + "/" + (day1-1) + "/" + year1;
 const currentDate1 = month1 + "/" + day1 + "/" + year1;
-const tomorrowsDate = month1 + "/" + (day1+1) + "/" + year1;
 //Print to page title
-monthAndYear.innerHTML = shiftFirstOutput(currentDate1);
-tomorrow.innerHTML = shiftFirstOutput(tomorrowsDate);
-yesterday.innerHTML = shiftFirstOutput(yesterdaysDate);
+monthAndYear.innerHTML = "Currently: " + shiftFirstOutput(currentDate1);
 
 //Alex shift calculation
 function getShift(checkDate)
@@ -51,12 +37,10 @@ function getShift(checkDate)
     }
     if(dayInCycle >= 0 && dayInCycle < 12)
     {
-        console.log(checkDate + "\n " + dayInCycle);
         return dayInCycle;
     }
 }
 function checkDate(){
-
     var m = document.getElementById("checkMonth");
     var monthText = m.options[m.selectedIndex].text;
     var d = document.getElementById("checkDay");
@@ -64,44 +48,15 @@ function checkDate(){
     var y = document.getElementById("checkYear");
     var yearText = y.options[y.selectedIndex].text;
 
-    let currentCheckDate = new Date(yearText, m.selectedIndex, dayText);
-    let yesterdayCheckDate = new Date(currentCheckDate);
-    let tomorrowCheckDate = new Date(currentCheckDate);
-
-    yesterdayCheckDate.setDate(currentCheckDate.getDate() - 1);
-    console.log("Yesterday: " + yesterdayCheckDate);
-    
-    tomorrowCheckDate.setDate(currentCheckDate.getDate() + 1);
-    console.log("Tomorrow: " + tomorrowCheckDate);
-
-    //checkLabel.innerHTML = "On " + currentCheckDate + " it will be Alex's " + shiftSecondOutput(currentCheckDate);
-    todayLabel.innerHTML = formatDateString(currentCheckDate);
-    yesterdayLabel.innerHTML = formatDateString(yesterdayCheckDate);
-    tomorrowLabel.innerHTML = formatDateString(tomorrowCheckDate);
-    monthAndYear.innerHTML = shiftFirstOutput(currentCheckDate);
-    tomorrow.innerHTML = shiftFirstOutput(tomorrowCheckDate);
-    yesterday.innerHTML = shiftFirstOutput(yesterdayCheckDate);
-
-}
-function getMonthIndex(month)
-{
-    switch(month)
-    {
-        case "July":
-            return 6;
-    }
-}
-function formatDateString(dateToConvert)
-{
-    let tring = dateToConvert.getFullYear() + "/" + (dateToConvert.getMonth() + 1) + "/" + dateToConvert.getDate();
-    return tring;
+    var currentCheckDate = monthText + "/" + dayText + "/" + yearText;
+    checkLabel.innerHTML = "On " + currentCheckDate + " it will be Alex's " + shiftSecondOutput(currentCheckDate);
 }
 function shiftFirstOutput(checkDate)
 {
     switch(getShift(checkDate))
     {
         case -1:
-            //document.getElementById("flashImage").src = vacay;
+            document.getElementById("flashImage").src = vacay;
             return "Vacation";
             break;
         case 1:
@@ -114,15 +69,15 @@ function shiftFirstOutput(checkDate)
             return "3rd Day Off";
             break;
         case 4:
-            //document.getElementById("flashImage").src = moon;
+            document.getElementById("flashImage").src = moon;
             return "1st Night Working";
             break;
         case 5:
-            //document.getElementById("flashImage").src = moon;
+            document.getElementById("flashImage").src = moon;
             return "2nd Night Working";
             break;
         case 6:
-            //document.getElementById("flashImage").src = moon;
+            document.getElementById("flashImage").src = moon;
             return "3rd Night Working";
             break;
         case 7:
@@ -135,15 +90,15 @@ function shiftFirstOutput(checkDate)
             return "3rd Day Off";
             break;
         case 10:
-            //document.getElementById("flashImage").src = sun;
+            document.getElementById("flashImage").src = sun;
             return "1st Day Working";
             break;
         case 11:
-            //document.getElementById("flashImage").src = sun;
+            document.getElementById("flashImage").src = sun;
             return "2nd Day Working";
             break;
         case 0:
-            //document.getElementById("flashImage").src = sun;
+            document.getElementById("flashImage").src = sun;
             return "3rd Day Working";
             break;
     }
@@ -153,7 +108,7 @@ function shiftSecondOutput(checkDate)
     switch(getShift(checkDate))
     {
         case -1:
-           // document.getElementById("flashImage").src = vacay;
+            document.getElementById("flashImage").src = vacay;
             return "Vacation";
             break;
         case 1:
@@ -166,15 +121,15 @@ function shiftSecondOutput(checkDate)
             return "3rd Day Off";
             break;
         case 4:
-           // document.getElementById("flashImage").src = moon;
+            document.getElementById("flashImage").src = moon;
             return "1st Night Working";
             break;
         case 5:
-            //document.getElementById("flashImage").src = moon;
+            document.getElementById("flashImage").src = moon;
             return "2nd Night Working";
             break;
         case 6:
-           // document.getElementById("flashImage").src = moon;
+            document.getElementById("flashImage").src = moon;
             return "3rd Night Working";
             break;
         case 7:
@@ -187,15 +142,15 @@ function shiftSecondOutput(checkDate)
             return "3rd Day Off";
             break;
         case 10:
-            //document.getElementById("flashImage").src = sun;
+            document.getElementById("flashImage").src = sun;
             return "1st Day Working";
             break;
         case 11:
-            //document.getElementById("flashImage").src = sun;
+            document.getElementById("flashImage").src = sun;
             return "2nd Day Working";
             break;
         case 0:
-            //document.getElementById("flashImage").src = sun;
+            document.getElementById("flashImage").src = sun;
             return "3rd Day Working";
             break;
     }
@@ -212,9 +167,8 @@ function checkAgainstVacation(checkDate)
         {
             return true;
         }
-        
+        return false;
     }
-    return false;
 }
 function daysInMonth(iMonth, iYear) {
     return 32 - new Date(iYear, iMonth, 32).getDate();
